f6fed9ea7ba24166c0cd2450b8345c3d94b1dd30
Thierry Carrez <thierry@openstack.org>
2011-07-07 16:00:41 +0200

    Retrieve milestone codename automatically, fix tempfile vuln

--- upload_release.py
+++ upload_release.py
@@ -3,21 +3,22 @@ import datetime
 import urllib
 import sys
 import subprocess
+import tempfile
 from launchpadlib.launchpad import Launchpad
 
 # Argument parsing
 if len(sys.argv) != 5:
     prog = sys.argv[0]
-    print >> sys.stderr, '''Upload a release tgz to a Launchpad project.
+    print >> sys.stderr, '''Grab tarball and release it on LP as milestone.
 
     Usage: %s <project> <version> <revision> <milestone>
-    Example: %s nova 2011.3~d2 20110702.r1234 diablo-3''' % (prog, prog)
+    Example: %s nova 2011.3 20110702.r1234 diablo-3''' % (prog, prog)
     sys.exit(1)
-    # TODO Get milestone codename from LP ?
 
 (project, version, revision, milestone) = sys.argv[1:]
 
 # Connect to LP
+print "Connecting to Launchpad..."
 try:
     launchpad = Launchpad.login_with('openstack-lp-scripts', 'production')
 except Exception, error:
@@ -25,30 +26,41 @@ except Exception, error:
     sys.exit(2)
 
 # Retrieve milestone
+print "Checking milestone..."
 lp_proj = launchpad.projects[project]
-for ms in lp_proj.all_milestones:
-    if ms.name == milestone:
-        lp_milestone = ms
+for lp_milestone in lp_proj.all_milestones:
+    if lp_milestone.name == milestone:
+        codename = lp_milestone.code_name.lower()
+        if len(codename) != 2:
+            print >> sys.stderr, 'Bad code name for milestone: %s' % codename
+            sys.exit(2)
         break
 else:
     print >> sys.stderr, 'Could not retrieve milestone'
     sys.exit(2)
 
-# Retrieve tgz
-base_tgz = "%s-%s~%s.tar.gz" % (project, version, revision)
+# Retrieve tgz, check contents and MD5
+print "Downloading tarball..."
+tmpdir = tempfile.mkdtemp()
+base_tgz = "%s-%s~%s~%s.tar.gz" % (project, version, codename, revision)
 url_tgz = "http://%s.openstack.org/tarballs/%s" % (project, base_tgz)
-(tgz, message) = urllib.urlretrieve(url_tgz)
+tgz = os.path.join(tmpdir, base_tgz)
 
-# Check and MD5 retrieved tgz
-subprocess.check_call(['tar', 'ztf', tgz])  # TODO could be less verbose
+(tgz, message) = urllib.urlretrieve(url_tgz, filename=tgz)
+
+try:
+    subprocess.check_call(['tar', 'ztf', tgz])
+except subprocess.CalledProcessError, e:
+    print >> sys.stderr, '%s is not a tarball !' % tgz
+    sys.exit(1)
 md5 = subprocess.check_output(['md5sum', tgz]).split()[0]
 
 # Sign tgz
+print "Signing tarball..."
 sig = tgz + '.asc'
 if not os.path.exists(sig):
     print 'Calling GPG to create tgz signature...'
     subprocess.check_call(['gpg', '--armor', '--sign', '--detach-sig', tgz])
-    # TODO fix tmpfile vuln !
 
 # Read contents
 with open(tgz) as tgz_file:
@@ -57,13 +69,15 @@ with open(sig) as sig_file:
     sig_content = sig_file.read()
 
 # Mark milestone released
+print "Marking milestone released..."
 release_notes = "This is the second milestone (diablo-2) on the road to Glance 2011.3."  # TODO fix message
 lp_release = lp_milestone.createProductRelease(
             date_released=datetime.datetime.utcnow(),
             release_notes=release_notes)
 
 # Upload file
-final_tgz = "%s-%s.tar.gz" % (project, version)
+print "Uploading release files..."
+final_tgz = "%s-%s~%s.tar.gz" % (project, version, codename)
 lp_file = lp_release.add_file(file_type='Code Release Tarball',
     description='%s "%s" milestone' % (project, milestone),
     file_content=tgz_content, filename=final_tgz,
@@ -71,6 +85,7 @@ lp_file = lp_release.add_file(file_type='Code Release Tarball',
     content_type="application/x-gzip; charset=binary")
 
 # Check LP-reported MD5
+print "Checking MD5s..."
 result_md5_url = "%s/+download/%s/+md5" % (lp_release.web_link, final_tgz)
 result_md5_file = urllib.urlopen(result_md5_url)
 result_md5 = result_md5_file.read().split()[0]
@@ -78,3 +93,4 @@ result_md5_file.close()
 if md5 != result_md5:
     print >> sys.stderr, 'MD5sums do not match !'
     sys.exit(3)
+print "Done!"
